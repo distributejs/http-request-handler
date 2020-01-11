1b78f2f064a1254877355ce6cf74de226d39136b
mmaciusowicz <mmaciusowicz@users.noreply.github.com>
2020-01-11 19:36:55 +0000

    Change indentation rule

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  collectCoverage: true,
-  coveragePathIgnorePatterns: ["/node_modules/"],
-  coverageReporters: ["text"],
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testPathIgnorePatterns: ["/node_modules/"]
+    collectCoverage: true,
+    coveragePathIgnorePatterns: ["/node_modules/"],
+    coverageReporters: ["text"],
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testPathIgnorePatterns: ["/node_modules/"]
 };
